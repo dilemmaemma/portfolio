1bf50dc1636ccb4a7a27b535c30e89899e793af2
delimmaemma <calvinhtml@gmail.com>
2023-10-08 00:04:40 -0500

    Got project page up and running

---
+++ src/frontend/components/pages/Project/Project.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import projects from '../../../data/projects';
+import '../../../styles/RenderLandingPage'
+
+return (
+    <div className='project-select'>
+        {projects.map((project) => (
+            <div
+                key={project.id}
+                className={`selection ${project.construction ? 'construction' : ''}` }
+                style={{ cursor: !project.construction ? 'pointer' : 'default' }} // If construction is false, able to click
+                onClick={!project.construction ? () => onClick(project.name) : null} // If construction is false, next statement runs
+            >
+                <div className='image-container'>
+                    <img
+                        className='image'
+                        src={project.image}
+                        alt={project.name}
+                    />
+                </div>
+                {project.construction && (
+                    <div className='construction-overlay'>
+                        <p className='construction-text'>Under Construction</p>
+                    </div>
+                )}
+                <div className='info-container'>
+                    <p className='title'>{project.name}</p>
+                    <p className='info' style={{ flexWrap: true }}>Description: {project.description}</p>
+                    <p className='info'>
+                        Languages: {project.languages > 1
+                        ? project.languages.map((language, index) => (
+                            <span key={index}>{language}{index !== project.languages.length - 1 ? '| ' : ''}</span>
+                        ))
+                        : project.languages}
+                    </p>
+                    <p className='info'>
+                        Notable Dependencies: {project.dependencies > 1
+                        ? project.dependencies.map((dependency, index) => (
+                            <span key={index}>{dependency}{index !== project.dependencies.length - 1 ? '| ' : ''}</span>
+                        ))
+                        : project.dependencies}
+                    </p>
+                    <p className='info'>Coded By: {project.creator}</p>
+                    <p className='info'>
+                        <a href={project.gitLink}>
+                            Go to GitHub Repository
+                        </a>
+                        &nbsp;
+                        <a href={project.downloadLink}>
+                            Download Project
+                        </a>
+                    </p>
+                </div>
+            </div>
+        ))}
+    </div>
+)
---
+++ src/frontend/data/projects.js
@@ -0,0 +1,40 @@
+import Minesweeper from '../images/minesweeper.png';
+
+export const programs = [
+    {
+        name: 'Minesweeper',
+        description: 'A classic game of minesweeper, coded in React.js',
+        image: Minesweeper,
+        creator: 'Emma and Jason',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'SQL'
+        ],
+        dependencies: [
+            'React.js',
+            'Node.js',
+            'Express.js',
+            'Axios',
+            
+        ],
+        gitLink: 'https://dilemmaemma.github.com/minesweeper/',
+        downloadLink: 'https://github.com/dilemmaemma/minesweeper/archive/master.zip'
+    },
+    {
+        name: 'HRF Asylum Grant Rate Tracker',
+        description: 'A React.js application that tracks the asylum grant rates in the United States',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Redux.js',
+            'Axios',
+            'Auth0',
+        ],
+    }
+]
