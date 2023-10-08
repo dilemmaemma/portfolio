0a75a61ecc2814460799c353b34018c04a0fd9f7
delimmaemma <calvinhtml@gmail.com>
2023-10-08 18:57:09 -0500

    Found new text color

--- src/frontend/styles/data_vis_colors.js
+++ src/frontend/styles/data_vis_colors.js
@@ -2,7 +2,8 @@ const colors = {
     background_color: '#7C8BAD',
     primary_accent_color: '#80B7DD',
     secondary_accent_color: '#5E82B1',
-    header_links: '#E2F0F7'
+    header_links: '#E2F0F7',
+    text_color: '#8C8EB2',
   };
   
   export { colors };
