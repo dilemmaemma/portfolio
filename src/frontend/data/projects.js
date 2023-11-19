0f3c1d1ebfe49fc8d176b8dc9a463b5300dcd4bc
delimmaemma <calvinhtml@gmail.com>
2023-11-19 11:38:07 -0600

    Added more projects

--- src/frontend/data/projects.js
+++ src/frontend/data/projects.js
@@ -43,6 +43,26 @@ export const projects = [
         downloadLink: 'https://github.com/dilemmaemma/asylum-rg-fe-starter/archive/master.zip'
     },
     {
+        name: 'React Weather App',
+        description: 'A React.js application that displays the current weather',
+        construction: true,
+        image: 'https://repository-images.githubusercontent.com/142146797/9465c500-1dd1-11eb-8e90-7f7d74182fb8',
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Axios',
+            'Open Weather Map API',
+            'Moment.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/react-weather-app',
+        downloadLink: 'https://github.com/dilemmaemma/react-weather-app/archive/master.zip'
+    },
+    {
         name: 'ELBC Website Reconstruction',
         description: "A reconstructed website for my parents' church",
         construction: true,
@@ -95,5 +115,271 @@ export const projects = [
         ],
         gitLink: 'https://github.com/dilemmaemma/Sprint15-Challenge',
         downloadLink: 'https://github.com/dilemmaemma/Sprint15-Challenge/archive/master.zip'
+    },
+    {
+        name: 'Cover Letter Generator',
+        description: 'A React.js application that generates a cover letter for a job application',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Node.js',
+            'Express.js',
+        ],
+        gitLink: 'https://github.com/dilemmaemma/cover-letter-generator',
+        downloadLink: 'https://github.com/dilemmaemma/cover-letter-generator/archive/master.zip'
+    },
+    {
+        name: 'Data Persistence and Authentication',
+        description: 'A backend application that helps users log in and store their information in a SQL database',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'Javascript',
+            'SQL'
+        ],
+        dependencies: [
+            'Node.js',
+            'Express.js',
+            'Knex.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/Sprint14-Challenge',
+        downloadLink: 'https://github.com/dilemmaemma/Sprint14-Challenge/archive/master.zip'
+    },
+    {
+        name: 'Node Database Practice',
+        description: 'A backend application that helps users log in and store their information in a SQL database to access recipe information',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'Javascript',
+            'SQL'
+        ],
+        dependencies: [
+            'Node.js',
+            'Express.js',
+            'Knex.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/node-db4-project',
+        downloadLink: 'https://github.com/dilemmaemma/node-db4-project/archive/master.zip'
+    },
+    {
+        name: 'Web API build',
+        description: 'Web API with CRUD functionality',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'Javascript'
+        ],
+        dependencies: [
+            'Node.js',
+            'Express.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/Sprint13-Challenge',
+        downloadLink: 'https://github.com/dilemmaemma/Sprint13-Challenge/archive/master.zip'
+    },
+    {
+        name: 'Heroku Deployable Express API',
+        description: 'A backend API that can be deployed on Heroku',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'Javascript'
+        ],
+        dependencies: [
+            'Node.js',
+            'Express.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/node-api4-project',
+        downloadLink: 'https://github.com/dilemmaemma/node-api4-project/archive/master.zip'
+    },
+    {
+        name: 'Article Creator',
+        description: 'A React.js application that helps users create articles using JWT authentication',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Axios'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/Sprint11-Challenge',
+        downloadLink: 'https://github.com/dilemmaemma/Sprint11-Challenge/archive/master.zip'
+    },
+    {
+        name: 'React Widgets Practice',
+        description: 'A React.js application that displays a list of widgets',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js'           
+        ],
+        gitLink: 'https://github.com/dilemmaemma/react-stateful-widgets',
+        downloadLink: 'https://github.com/dilemmaemma/react-stateful-widgets/archive/master.zip'
+    },
+    {
+        name: 'React Movie App Practice',
+        description: 'A React.js application that displays a list of movies, implementing CRUD, HTTP, and AJAX',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Axios'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-HTTP',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-HTTP/archive/master.zip'
+    },
+    {
+        name: 'Client Authentication Practice',
+        description: 'A React.js frontend application that uses JWT authentication',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Axios'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-client-auth',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-client-auth/archive/master.zip'
+    },
+    {
+        name: 'React Testing Practice',
+        description: 'An application that tests the functionality using Jest',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'Jest'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-testing-react',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-testing-react/archive/master.zip'
+    },
+    {
+        name: 'Advanced React State',
+        description: 'An application that uses React state and props',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Axios',
+            'Redux'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/Sprint10-Challenge',
+        downloadLink: 'https://github.com/dilemmaemma/Sprint10-Challenge/archive/master.zip'
+    },
+    {
+        name: 'React Context API Practice',
+        description: 'An application that uses React Context API',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-context',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-context/archive/master.zip'
+    },
+    {
+        name: 'React Async Redux Practice',
+        description: 'An application that uses React Async Redux',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Redux'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-async-redux',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-async-redux/archive/master.zip'
+    },
+    {
+        name: 'React Redux Practice',
+        description: 'An application that uses React Redux',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Redux'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-redux',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-redux/archive/master.zip'
+    },
+    {
+        name: 'Reducer Practice',
+        description: 'An application that uses Redux reducers',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js',
+            'Redux'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/web-module-project-reducer',
+        downloadLink: 'https://github.com/dilemmaemma/web-module-project-reducer/archive/master.zip'
+    },
+    {
+        name: 'Advanced React',
+        description: 'An application that uses React state and props',
+        construction: false,
+        creator: 'Emma',
+        languages: [
+            'HTML',
+            'CSS',
+            'JavaScript'
+        ],
+        dependencies: [
+            'React.js'
+        ],
+        gitLink: 'https://github.com/dilemmaemma/Sprint9-Challenge',
+        downloadLink: 'https://github.com/dilemmaemma/Sprint9-Challenge/archive/master.zip'
     }
 ]
